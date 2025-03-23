e0e116e010c5443da292012146ff1bb65ff77b12
Semyashkina <forty.two2010@gmail.com>
2025-03-23 14:08:41 +0300

    update gitignore

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,11 +7,11 @@ import _ from 'lodash';
 
 const program = new Command();
 
-const genDiff = (path10, path20) => {
-  const path1 = path.resolve(process.cwd(), path10);
-  const path2 = path.resolve(process.cwd(), path20);
-  const file1 = parse(path1);
-  const file2 = parse(path2);
+const genDiff = (path1, path2) => {
+  const filepath1 = path.resolve(process.cwd(), path1);
+  const filepath2 = path.resolve(process.cwd(), path2);
+  const file1 = parse(filepath1);
+  const file2 = parse(filepath2);
   const keys = _.union(Object.keys(file1), Object.keys(file2)).sort();
   const diff = `{\n${keys.reduce((acc, key) => {
     if (Object.hasOwn(file1, key) && !Object.hasOwn(file2, key)) {
@@ -28,7 +28,7 @@ const genDiff = (path10, path20) => {
       return acc;
     }
   }, '')}}`;
-  console.log(diff);
+  return diff;
 };
 
 program
@@ -36,7 +36,10 @@ program
   .option('-V, --version ', 'output the version number')
   .option('-f, --format [type]', 'output format')
   .arguments('<filepath1> <filepath2>')
-  .action((filepath1, filepath2) => genDiff(filepath1, filepath2));
+  .action((filepath1, filepath2) => {
+    const difference = genDiff(filepath1, filepath2);
+    console.log(difference);
+  });
 
 program.parse();
 
