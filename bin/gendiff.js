8f13a2f2a9885d0f7f8acf1cd341c88fa097e311
Ogurchik00 <nik.skripka.000@gmail.com>
2025-04-13 00:48:01 +0900

    the remaining logic of the project has been completed

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,13 +1,18 @@
 #!/usr/bin/env node
-import { Command } from "commander";
+import { Command } from 'commander';
+import app from '../src/index.js'
 
-const program = new Command
+const program = new Command();
 
-program
-  .name('gendiff')
-  .description(' Compares two configuration files and shows a difference.')
-  .version('1.0.0')
-  .option('-f, --format <type>', 'output format')
-  .argument('<filepath1>')
-  .argument('<filepath2>')
-  program.parse();
+const gendiff = program
+    .name('gendiff')
+    .argument('<filepath1>')
+    .argument('<filepath2>')
+    .description(`Compares two configuration files and shows a difference.`)
+    .action((a, b, options) => {
+        console.log(app(a, b, options));
+    })
+    .option('-V, --version', 'output the version number')
+    .option('-f, --format [type]', 'output format');
+
+gendiff.parse();
